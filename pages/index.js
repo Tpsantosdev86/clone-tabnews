6fb43c74020f861aafde3ef6270f236c13310365
Tpsantosdev86 <thiagosantosdev86@gmail.com>
2026-07-22 22:15:56 -0300

    modifição na frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-      <h1>"Quem encontra uma esposa encontra algo excelente; recebeu uma bênção do Senhor." Te Amo!</h1>
+      <h1>"Quem encontra uma esposa encontra algo excelente; recebeu uma bênção do Senhor." Te Amo!!!</h1>
   );
 }
 
