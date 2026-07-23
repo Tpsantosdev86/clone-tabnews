a1555fd51c4da233f5c15f67a238d993caea1310
Tpsantosdev86 <thiagosantosdev86@gmail.com>
2026-07-22 22:50:33 -0300

    alteração texto e add gif.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,50 @@
-function Home() {
-  return (
-      <h1>"Quem encontra uma esposa encontra algo excelente; recebeu uma bênção do Senhor." Te Amo!!!</h1>
-  );
-}
+ import Script from "next/script";
 
-export default Home;
+  function Home() {
+    return (
+      <>
+        <main>
+          <h1>Quem encontra uma esposa encontra algo excelente; recebeu uma
+            bênção do Senhor. Te Amo!!!
+          </h1>
+
+          <div
+            className="tenor-gif-embed"
+            data-postid="2714484565602153012"
+            data-share-method="host"
+            data-aspect-ratio="0.928571"
+            data-width="180px"
+          >
+            <a href="https://tenor.com/pt-BR/view/bubu-bubu-dudu-love-cute-panda-gif-2714484565602153012">
+              Fric Sticker
+            </a>
+          </div>
+        </main>
+
+        <Script
+          src="https://tenor.com/embed.js"
+          strategy="afterInteractive"
+        />
+
+        <style jsx>{`
+          main {
+            min-height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 20px;
+            background-color: #fce4ec;
+          }
+
+          h1 {
+            color: #8e244d;
+            font-family: Georgia, "Times New Roman", serif;
+            text-align: center;
+            -webkit-text-stroke: 1px black;
+          }
+        `}</style>
+      </>
+    );
+  }
+
+  export default Home;
